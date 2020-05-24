0225943ef62d9cfb2ce08452ca7030041cd249e2
M4ntha5 <mantas.daunoravicius@ktu.edu>
2020-05-24 14:31:08 +0300

    nav test

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -2,7 +2,8 @@
 <div>
        <nav class="navbar navbar-expand-lg navbar-light bg-light my-nav">
             <router-link to="/" class="navbar-brand">CmApp</router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" data-toggle="collapse" 
+                  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent">
                   <span class="navbar-toggler-icon"></span>
             </button>
 
